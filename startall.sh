eea21fbe903a04db77f114c5e0a443965c583001
Christian Flodihn <christian@flodihn.se>
2013-06-17 11:34:54 +0100

    Added script for starting all servers.

---
+++ startall.sh
@@ -0,0 +1,17 @@
+cd ConnectionServer
+sh start.sh -noshell &
+cd ..
+
+cd AccountServer 
+sh start.sh -noshell &
+cd ..
+
+cd CharacterServer 
+sh start.sh -noshell &
+cd ..
+
+cd AreaServer 
+sh start.sh -noshell &
+cd ..
+
+
