4c9dc41203bce934da1fab8d26c97f9b1ddaa304
Christian Flodihn <christian@flodihn.se>
2014-08-25 17:07:34 +0200

    Fixed an issue with the clients.

--- TestSuite/src/recv_loop.erl
+++ TestSuite/src/recv_loop.erl
@@ -18,33 +18,39 @@ recv_loop(Owner, Socket, #recv_state{id=Id, bytes_recv=BytesRecv,
     receive 
         {get_state} ->
             Owner ! State;
-        {tcp, Socket, <<?NOTIFY_PONG, BinTime/binary>> = Data} ->
-            TimeStamp = binary_to_term(BinTime),
-            Diff = timer:now_diff(now(), TimeStamp),
-            %io:format("Got ping ms: ~p.~n", [Diff/1000]),
-            NewBytesRecv = BytesRecv + byte_size(Data) + 2,
-            recv_loop(Owner, Socket, State);
-        {tcp, Socket, <<?OBJ_DIR, IdLen/integer, Id:IdLen/binary, 
-            X/little-float, Y/little-float, Z/little-float, 
-            BinTime/binary>> = Data} ->
-            Time = binary_to_term(BinTime),
-            Diff = timer:now_diff(now(), Time),
-            %io:format("OBJ_DIR ms: ~p.~n", [Diff/1000]),
-            NewBytesRecv = BytesRecv + byte_size(Data) + 2,
-            recv_loop(Owner, Socket, State);
-        {tcp, Socket, <<?OBJ_SPEED, IdLen/integer, Id:IdLen/binary,
-            Speed/little-float, 
-            BinTimeStamp/binary>> = Data} ->
-            TimeStamp = binary_to_term(BinTimeStamp),
-            Diff = timer:now_diff(now(), TimeStamp),
-            NewBytesRecv = BytesRecv + byte_size(Data) + 2,
-            %io:format("OBJ_SPEED ms: ~p.~n", [Diff/1000]),
-            recv_loop(Owner, Socket, State);
+        %{tcp, Socket, <<?NOTIFY_PONG, BinTime/binary>> = Data} ->
+        %    %TimeStamp = binary_to_term(BinTime),
+        %    %Diff = timer:now_diff(now(), TimeStamp),
+        %    %io:format("Got ping ms: ~p.~n", [Diff/1000]),
+        %    NewBytesRecv = BytesRecv + byte_size(Data) + 2,
+		%	recv_loop(Owner, Socket, 
+        %        State#recv_state{bytes_recv=NewBytesRecv, 
+        %        cmds_recv=CmdsRecv + 1});
+        %{tcp, Socket, <<?OBJ_DIR, IdLen/integer, Id:IdLen/binary, 
+        %    X/little-float, Y/little-float, Z/little-float, 
+        %    BinTime/binary>> = Data} ->
+        %    %Time = binary_to_term(BinTime),
+        %    %Diff = timer:now_diff(now(), Time),
+        %    %io:format("OBJ_DIR ms: ~p.~n", [Diff/1000]),
+        %    NewBytesRecv = BytesRecv + byte_size(Data) + 2,
+		%	recv_loop(Owner, Socket, 
+        %        State#recv_state{bytes_recv=NewBytesRecv, 
+        %        cmds_recv=CmdsRecv + 1});
+        %{tcp, Socket, <<?OBJ_SPEED, IdLen/integer, Id:IdLen/binary,
+        %    Speed/little-float, 
+        %    %BinTimeStamp/binary>> = Data} ->
+        %    %TimeStamp = binary_to_term(BinTimeStamp),
+        %    %Diff = timer:now_diff(now(), TimeStamp),
+        %    NewBytesRecv = BytesRecv + byte_size(Data) + 2,
+        %    %io:format("OBJ_SPEED ms: ~p.~n", [Diff/1000]),
+		%	recv_loop(Owner, Socket, 
+        %        State#recv_state{bytes_recv=NewBytesRecv, 
+        %        cmds_recv=CmdsRecv + 1});
         {tcp, Socket, Data} ->
             NewBytesRecv = BytesRecv + byte_size(Data) + 2,
             recv_loop(Owner, Socket, 
                 State#recv_state{bytes_recv=NewBytesRecv, 
-                    cmds_recv=CmdsRecv + 1});
+                cmds_recv=CmdsRecv + 1});
         Other ->
             error_logger:error_report([{unknown_data, Other}])
     after
