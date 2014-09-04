459cb39f959a7729858a8f960a84fe2b90917041
Christian Flodihn <christian@flodihn.se>
2014-09-04 14:25:58 +0200

    Finnaly fixed the issues with no char and account login.

--- ConnectionServer/src/connection.erl
+++ ConnectionServer/src/connection.erl
@@ -18,7 +18,6 @@
 
 % API
 -export([
-    conn_reply/2,
     socket_send/2
     ]).
 
@@ -38,7 +37,19 @@ start_link(Socket) ->
 
 init(Socket) ->
     State = #state{socket=Socket},
-	{ok, State}.
+    {ok, DefaultAreaSrv} = application:get_env(start_area),
+    error_logger:info_report({char_login, DefaultAreaSrv}),
+    rpc:call(DefaultAreaSrv, libplayer_srv, create, [self()]),
+    receive
+        {char_login, {pid, Pid}, {id, Id}} ->
+            CharInfo = #charinfo{id=Id, pid=Pid},
+            NewState = State#state{charinfo=CharInfo},
+            IdLen = byte_size(Id),
+            socket_send(Socket, <<?CHAR_LOGIN_SUCCESS, IdLen, Id/binary>>),
+            {ok, playing, NewState};
+        Error ->
+            {stop, player_login, State)
+    end.
 
 handle_event(Event, StateName, StateData) ->
     error_logger:info_report([{event, Event, StateName}]),
@@ -90,10 +101,6 @@ code_change(_OldVsn, _StateName, _StateData, _Extra) ->
 
 terminate(_Reason, _StateName, _StateData) ->
     ok.
-
-conn_reply(Socket, Reply) ->
-    %error_logger:info_report([{debug, conn_reply, Reply}]),
-    gen_tcp:send(Socket, term_to_binary(Reply)).
 
 socket_send(Socket, Msg) ->
     %error_logger:info_report([{socket_send, Msg, byte_size(Msg)}]),
