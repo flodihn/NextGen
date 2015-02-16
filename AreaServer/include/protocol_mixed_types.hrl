f673361f3cf1ce3fd62c08b23f4c0554c6581f29
Christian Flodihn <christian@flodihn.se>
2015-02-16 14:09:56 +0100

    Added missing header file.

---
+++ AreaServer/include/protocol_mixed_types.hrl
@@ -0,0 +1,8 @@
+% Defines for the mixed type.
+-define(MIXED_CHAR, 0).
+-define(MIXED_INTEGER, 1).
+-define(MIXED_FLOAT, 2).
+-define(MIXED_STRING, 3).
+-define(MIXED_LIST, 4).
+-define(MIXED_VECTOR, 5).
+
